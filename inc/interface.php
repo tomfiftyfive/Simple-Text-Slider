e23cbfef9b4a57d7bce7267e4787f066d68fb20f
Tom <mail@tom-henneken.de>
2016-08-17 10:37:14 +0200

    alpha color picker added

--- inc/interface.php
+++ inc/interface.php
@@ -8,11 +8,27 @@ function simpleTextSlider_action_links( $links ) {
    return $links;
 }
 
+// backend scripts and styles
+add_action( 'admin_enqueue_scripts', 'simpleTextSlider_backend_files' );
+
+function simpleTextSlider_backend_files( $hook ) { 
+    if( is_admin() ) {
+        
+        // color picker js and css
+        wp_enqueue_script( 'alpha-color-picker', plugins_url( '../js/alpha-color-picker.js', __FILE__ ), array( 'wp-color-picker' ), null, true );
+        
+        wp_enqueue_style( 'alpha-color-picker', plugins_url( '../css/alpha-color-picker.css', __FILE__ ), array( 'wp-color-picker' ));
+ 
+        // interface js
+        wp_enqueue_script( 'custom-script-handle', plugins_url( '../js/simpleTs_backend_scripts.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+    }
+}
+
+// init menu and settings
 add_action( 'admin_menu', 'simpleTextSlider_add_admin_menu' );
 add_action( 'admin_init', 'simpleTextSlider_settings_init' );
 
 function simpleTextSlider_add_admin_menu(  ) { 
-
 	add_options_page( 'Simple Text Slider', 'Simple Text Slider', 'manage_options', 'simple_text_slider', 'simpleTextSlider_options_page' );
 }
 
@@ -44,15 +60,14 @@ function simpleTextSlider_settings_init(  ) {
 //	);
 }
 
-
+// render interface output
 function simpleTextSlider_text_field_0_render(  ) { 
 
 	$options = get_option( 'simpleTextSlider_settings' );
 	?>
-	<input type='text' name='simpleTextSlider_settings[simpleTextSlider_text_field_0]' value='<?php echo $options['simpleTextSlider_text_field_0']; ?>'>
+	<input type="text" class="alpha-color-picker" name='simpleTextSlider_settings[simpleTextSlider_text_field_0]'  value='<?php echo $options['simpleTextSlider_text_field_0']; ?>' data-default-color="#222" data-show-opacity="true" />
 	<?php
 }
-
 
 function simpleTextSlider_textarea_field_1_render(  ) { 
 
@@ -63,7 +78,6 @@ function simpleTextSlider_textarea_field_1_render(  ) {
  	</textarea>
 	<?php
 }
-
 
 function simpleTextSlider_settings_section_callback(  ) { 
 
