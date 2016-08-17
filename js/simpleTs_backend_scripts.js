fed2f6d9a3013fd1d5157a536e3a37355f4f3d19
Tom <mail@tom-henneken.de>
2016-08-17 18:54:39 +0200

    color picker absolute position

--- js/simpleTs_backend_scripts.js
+++ js/simpleTs_backend_scripts.js
@@ -1,3 +1,9 @@
 jQuery( document ).ready( function( $ ) {
     $( 'input.alpha-color-picker' ).alphaColorPicker();
+    
+    var alphaColorZindex = 100;
+    $('.wp-picker-container').each(function() {
+        alphaColorZindex--;
+        $(this).css('z-index', alphaColorZindex);
+    });
 });
